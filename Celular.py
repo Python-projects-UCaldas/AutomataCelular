97923d89b9a4a78faa7714c3acb80bae217356e0
LucasBMax <lucas.1701716343@gmail.com>
2020-03-03 16:19:35 -0500

    interface

--- Celular.py
+++ Celular.py
@@ -1,85 +1,145 @@
 import sys
-print(sys.path)
 import pygame
 import itertools
 import random
 import time
 from threading import Thread
+import sys
+import pygame
+import itertools
+import random
+import time
+from threading import Thread
+from PyQt5.QtWidgets import QApplication,QMainWindow,QMessageBox
+from PyQt5 import uic, QtCore, Qt
 
+##############################INTERFACE##############################
+
+class Ventana(QMainWindow):
+	
+	def __init__(self):
+	
+		#iniciar pbjeto QMainWindow
+		QMainWindow.__init__(self)
+		#Cargar archivo
+		uic.loadUi("Automata.ui",self)
+		
+		self.initUI()
+
+
+	def initUI(self):
+		self.inputRegla.text()
+		self.regla.clicked.connect(lambda:self.metodo())
+		
+
+
+##############################METODOS##############################
+
+'''
+def convertTobase (num, base):
+    if num == 0:
+        return '0'
+    nums = []
+    while num:
+        num, r = divmod(num, base)
+        nums.append(str(r))
+    return  ''.join(reversed(nums))
+
+regla = convertTobase(25,5)
+´'''
 iter = [0, 1, 2, 3, 4]
-x = itertools.combinations_with_replacement(iter, 3)
+limit = 27 #input
+rule = [random.randint(0,len(iter) - 1) for i in (range(0, limit))]
+x = itertools.product(iter, repeat=3)
 x = set(x)
-piano = [random.randint(0, 4) for i in range(20)]
-bateria = [random.randint(0, 4) for i in range(20)]
-d = {valor: random.randint(0, 4) for valor in sorted(x)}
+piano = [[]]
+drums = [[]]
+d = dict(zip(sorted(x),rule))
+print(rule)
+print(d)
 
+##############################GRAFICOS##############################
+
+
+# Definimos algunos colores
+NEGRO = (0, 0, 0)
+BLANCO = (255, 0, 255)
+VERDE = ( 0, 255, 0)
+ROJO = (255, 0, 0)
+ 
+# Establecemos el LARGO y ALTO de cada celda de la retícula.
+LARGO  = 20
+ALTO = 20
+ 
+# Establecemos el margen entre las celdas.
+MARGEN = 5
+ 
+# Creamos un array bidimensional. Un array bidimensional
+# no es más que una lista de listas.
+grid = []
+for fila in range(10):
+    # Añadimos un array vacío que contendrá cada celda 
+    # en esta fila
+    grid.append([])
+    for columna in range(10):
+        grid[fila].append(0) # Añade una celda
+ 
+# Establecemos la fila 1, celda 5 a uno. (Recuerda, los números de las filas y
+# columnas empiezan en cero.)
+grid[1][5] = 1
+ 
+# Inicializamos pygame
 pygame.init()
-background_colour = (255,255,255)
-(width, height) = (800, 600)
-
-a4 = pygame.mixer.Sound('C:\\Users\\pista\\Desktop\\automatas-master\\Sounds\\Piano-A4.ogg')
-c4 = pygame.mixer.Sound('C:\\Users\\pista\\Desktop\\automatas-master\\Sounds\\Piano-C4.ogg')
-d4 = pygame.mixer.Sound('C:\\Users\\pista\\Desktop\\automatas-master\\Sounds\\Piano-D#4.ogg')
-f4 = pygame.mixer.Sound('C:\\Users\\pista\\Desktop\\automatas-master\\Sounds\\Piano-F#4.ogg')
-'''
-a7 = pygame.mixer.Sound('C:\\Users\\pista\\Desktop\\automatas-master\\Sounds\\t1.mp3')
-c7 = pygame.mixer.Sound('C:\\Users\\pista\\Desktop\\automatas-master\\Sounds\\t2.mp3')
-d7 = pygame.mixer.Sound('C:\\Users\\pista\\Desktop\\automatas-master\\Sounds\\t3.mp3')
-f7 = pygame.mixer.Sound('C:\\Users\\pista\\Desktop\\automatas-master\\Sounds\\t4.mp3')
-'''
-def play_piano():
-  print("piano")
-  if len(piano) > 0:
-    note = piano.pop()
-    if note == 1:
-      print("a4")
-      a4.play()
-    if note == 2:
-      c4.play()
-      print("c4")
-    if note == 3:
-      d4.play()
-      print("d4")
-    if note == 4:
-      f4.play()
-      print("f4")
-  else:
-    note = 0
-'''
-def play_drums():
   
-  if len(bateria) > 0:
-    note = bateria.pop()
-    if note == 1:
-      print("a4")
-      a7.play()
-    if note == 2:
-      c7.play()
-      print("c4")
-    if note == 3:
-      d7.play()
-      print("d4")
-    if note == 4:
-      f7.play()
-      print("f4")
-  else:
-    note = 0
-'''
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Tutorial 1')
-screen.fill(background_colour)
-pygame.display.flip()
-running = True
-#vec = vec.reverse()
-#hilo1 = Thread(target=play_drums)
-hilo2 = Thread(target=play_piano)
-
-while running:
-  time.sleep(0.5)
-  play_piano()
-  #play_drums()
-  
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-
+# Establecemos el LARGO y ALTO de la pantalla
+DIMENSION_VENTANA = [255, 255]
+pantalla = pygame.display.set_mode(DIMENSION_VENTANA)
+ 
+# Establecemos el título de la pantalla.
+pygame.display.set_caption("Retículas y Matrices")
+ 
+# Iteramos hasta que el usuario pulse el botón de salir.
+hecho = False
+ 
+# Lo usamos para establecer cuán rápido de refresca la pantalla.
+reloj = pygame.time.Clock()
+ 
+# -------- Bucle Principal del Programa-----------
+while not hecho:
+    for evento in pygame.event.get(): 
+        if evento.type == pygame.QUIT: 
+            hecho = True
+        elif evento.type == pygame.MOUSEBUTTONDOWN:
+            # El usuario presiona el ratón. Obtiene su posición.
+            pos = pygame.mouse.get_pos()
+            # Cambia las coordenadas x/y de la pantalla por coordenadas reticulares
+            columna = pos[0] // (LARGO + MARGEN)
+            fila = pos[1] // (ALTO + MARGEN)
+            # Establece esa ubicación a cero
+            grid[fila][columna] = 1
+            print("Click ", pos, "Coordenadas de la retícula: ", fila, columna)
+ 
+    # Establecemos el fondo de pantalla.
+    pantalla.fill(NEGRO)
+ 
+    # Dibujamos la retícula
+    for fila in range(10):
+        for columna in range(10):
+            color = BLANCO
+            if grid[fila][columna] == 1:
+                color = VERDE
+            pygame.draw.rect(pantalla,
+                             color,
+                             [(MARGEN+LARGO) * columna + MARGEN,
+                              (MARGEN+ALTO) * fila + MARGEN,
+                              LARGO,
+                              ALTO])
+     
+    # Limitamos a 60 fotogramas por segundo.
+    reloj.tick(60)
+ 
+    # Avanzamos y actualizamos la pantalla con lo que hemos dibujado.
+    pygame.display.flip()
+     
+# Pórtate bien con el IDLE.
+pygame.quit()
